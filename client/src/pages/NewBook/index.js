a7daf87cb49b0dea31d39ff27f3b775bdb481d83
leandrocgsi <leandrocgsi@gmail.com>
2022-02-17 14:28:51 -0300

    Recovering Book to Edit

--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useHistory, useParams, Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 
@@ -10,6 +10,7 @@ import logoImage from '../../assets/logo.svg';
 
 export default function NewBook(){
 
+    const [id, setId] = useState(null);
     const [author, setAuthor] = useState('');
     const [launchDate, setLaunchDate] = useState('');
     const [price, setPrice] = useState('');
@@ -26,6 +27,28 @@ export default function NewBook(){
             Authorization: `Bearer ${accessToken}`
         }
     };
+
+    useEffect(() => {
+        if (bookId === '0') return;
+        else loadBooks();
+    }, [bookId]);
+
+    async function loadBooks() {
+        try {
+            const response = await api.get(`api/book/v1/${bookId}`, authorization) ;
+
+            let adjustedDate = response.data.launchDate.split("T", 10)[0];
+
+            setId(response.data.id);
+            setTitle(response.data.title);
+            setAuthor(response.data.author);
+            setPrice(response.data.price);
+            setLaunchDate(adjustedDate);
+        } catch (err) {
+            alert('Error recovering Book! Try again!');
+            history.push('/books');
+        }
+    }
 
     async function createNewBook(e){
         e.preventDefault();
@@ -51,8 +74,8 @@ export default function NewBook(){
             <div className="content">
                 <section className="form">
                     <img src={logoImage} alt="Erudio"/>
-                    <h1>Add New Book</h1>
-                    <p>Enter the book information and click on 'Add'! #### {bookId}</p>
+                    <h1>{bookId === '0' ? 'Add New' : 'Update'} Book</h1>
+                    <p>Enter the book information and click on {bookId === '0' ? `'Add New'` : `'Update'`}!</p>
                     <Link className="back-link" to="/books">
                         <FiArrowLeft size={16} color="#251fc5"/>
                         Home
@@ -80,7 +103,7 @@ export default function NewBook(){
                         onChange={e => setPrice(e.target.value)}
                     />
 
-                    <button className="button" type="submit">Add</button>
+                    <button className="button" type="submit">{bookId === '0' ? 'Add' : 'Update'}</button>
                 </form>
             </div>
         </div>
