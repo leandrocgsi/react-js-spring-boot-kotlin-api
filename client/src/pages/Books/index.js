7db90961c4bcfbc1409bcc89827ffcb7ad1ed657
leandrocgsi <leandrocgsi@gmail.com>
2022-02-17 11:32:10 -0300

    Connecting NewBook and ListBooks Pages to API

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useHistory, Link } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg'
 
 export default function Books(){
+
+    const [books, setBooks] = useState([]);
+
+    const history = useHistory();
+    
+    const accessToken = localStorage.getItem('accessToken');
+    const username = localStorage.getItem('username');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    useEffect(() => {
+        api.get('api/book/v1', authorization)
+            .then(response => {
+                setBooks(response.data._embedded.bookVOes);
+            });
+    }, [accessToken])
+
     return (
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="Erudio"/>
-                <span>Welcome, <strong>Leandro</strong>!</span>
+                <span>Welcome, <strong>{username.toUpperCase()}</strong>!</span>
                 <Link className="button" to="book/new">Add New Book</Link>
                 <button type="button">
                     <FiPower size={18} color="#251FC5" />
@@ -20,96 +43,27 @@ export default function Books(){
 
             <h1>Registered Books</h1>
             <ul>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-                    
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
+                {books.map(book =>(
+                    <li key={book.id}>
+                        <strong>Title:</strong>
+                        <p>{book.title}</p>
+                        <strong>Author:</strong>
+                        <p>{book.author}</p>
+                        <strong>Price:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
+                        <strong>Release Date:</strong>
+                        <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
+                        
+                        <button type="button">
+                            <FiEdit size={20} color="#251FC5"/>
+                        </button>
+                        
+                        <button type="button">
+                            <FiTrash2 size={20} color="#251FC5"/>
+                        </button>
+                    </li>
+                ))}
+                
             </ul>
         </div>
     );
--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {useHistory, Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 
+import api from '../../services/api'
 
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
 
 export default function NewBook(){
+
+    const [author, setAuthor] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    const history = useHistory();
+    
+    const accessToken = localStorage.getItem('accessToken');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    async function createNewBook(e){
+        e.preventDefault();
+
+        const data = {
+            author,
+            launchDate,
+            price,
+            title
+        };
+
+        try {
+            await api.post('api/book/v1', data, authorization) ;
+            history.push('/books');
+        } catch (error) {
+            alert('Login failed! Try again!');
+        }
+        history.push('/books');
+    };
+
     return (
         <div className="new-book-container">
             <div className="content">
@@ -20,11 +56,27 @@ export default function NewBook(){
                         Home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Title" />
-                    <input placeholder="Author" />
-                    <input type="date" />
-                    <input placeholder="Price" />
+                <form onSubmit={createNewBook}>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <input
+                        placeholder="Author"
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        value={launchDate}
+                        onChange={e => setLaunchDate(e.target.value)}
+                    />
+                    <input
+                        placeholder="Price"
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Add</button>
                 </form>
